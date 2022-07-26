f7e72e2a47c4da25487e4478e259043216566ea9
xiebiao360 <xiebiao360@outlook.com>
2022-07-26 23:11:56 +0800

    添加项目文件。

---
+++ WlxsDbTransfer/Program.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace WlxsDbTransfer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "server=127.0.0.1;uid=root;pwd=xie360360;database=wlxs_copy";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                // Do work here.  
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
